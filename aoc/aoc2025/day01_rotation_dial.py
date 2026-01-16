f8acf5196d745a3c5b53ea3ab43ef03817c98634
Alina Artcibasova <alina.artcibasova@gmail.com>
2026-01-16 09:16:06 +0000

    refactor: update day01 code

--- aoc/aoc2025/day01_rotation_dial.py
+++ aoc/aoc2025/day01_rotation_dial.py
@@ -1,67 +1,74 @@
-def read_data(file_name):
-    with open(file_name) as my_file:
-        lines = my_file.readlines()
+DIAL_MIN = 0
+DIAL_MAX = 99
+DIAL_SIZE = DIAL_MAX + 1
 
-    lines = [line.replace("\n", "") for line in lines]
+def read_data_01(file_name: str) -> list[str]:
+    """Read input data for day01."""
+    with open(file_name) as my_file:
+        input = my_file.read()
+    lines = input.splitlines()
 
     return lines
 
-
-def day01_1(current_position, lines):
+def day01_1(current_position: int, lines: list[str]) -> tuple[list[tuple[str, int]], int]:
+    """Solve first part of the puzzle for day01."""
     zero_counter = 0
     positions = []
     for line in lines:
         if "L" in line:
-            increment = int(line.replace("L", "")) % 100
+            increment = int(line.replace("L", "")) % DIAL_SIZE
             new_position = current_position - increment
         if "R" in line:
-            increment = int(line.replace("R", "")) % 100
+            increment = int(line.replace("R", "")) % DIAL_SIZE
             new_position = current_position + increment
 
-        if new_position < 0:
-            current_position = abs(99 + new_position + 1)
-        elif new_position > 99:
-            current_position = abs(99 - new_position + 1)
+        if new_position < DIAL_MIN:
+            current_position = abs(DIAL_MAX + new_position + 1)
+        elif new_position > DIAL_MAX:
+            current_position = abs(DIAL_MAX - new_position + 1)
         else:
             current_position = new_position
 
         positions.append((line, current_position))
 
-        if current_position == 0:
+        if current_position == DIAL_MIN:
             zero_counter += 1
 
     return positions, zero_counter
 
 
-def day01_2(current_position, lines):
+def day01_2(current_position: int, lines: list[str]) -> tuple[list[tuple[str, int]], int]:
+    """Solve second part of the puzzle for day01."""
     zero_counter = 0
     positions = []
     for line in lines:
         if "L" in line:
             change = int(line.replace("L", ""))
-            increment = -(change % 100)
+            increment = - ( change % DIAL_SIZE )
         if "R" in line:
             change = int(line.replace("R", ""))
-            increment = change % 100
+            increment = change % DIAL_SIZE
         new_position = current_position + increment
-        zero_counter += change // 100
+        zero_counter += change // DIAL_SIZE
 
-        if new_position < 0:
+        if new_position < DIAL_MIN:
             if current_position != 0:
                 zero_counter += 1
-            current_position = abs(99 + new_position + 1)
+            current_position = abs(DIAL_MAX + new_position + 1)
 
-        elif new_position > 99:
-            current_position = abs(99 - new_position + 1)
+        elif new_position > DIAL_MAX:
+            current_position = abs(DIAL_MAX - new_position + 1)
             if current_position != 0:
                 zero_counter += 1
         else:
             current_position = new_position
 
-        if current_position == 0:
+
+        if current_position == DIAL_MIN:
             zero_counter += 1
 
         positions.append((line, current_position, zero_counter))
+
 
     return positions, zero_counter
 
@@ -69,27 +76,28 @@ def day01_2(current_position, lines):
 if __name__ == "__main__":
     current_position = 50
 
-    file_name = "data/input01_test.txt"
-    lines = read_data(file_name)
+    file_name = "data/aoc2025/input01_test.txt"
+    lines = read_data_01(file_name)
+    # print(lines)
 
     positions, zero_counter = day01_1(current_position, lines)
     print("====PART 1====")
     print(positions)
     print(zero_counter)
 
-    positions, zero_counter = day01_2(current_position, lines)
-    print("====PART 2====")
-    print(positions)
-    print(zero_counter)
-
-    file_name = "data/input01_puzzle.txt"
-    lines = read_data(file_name)
-    positions, zero_counter = day01_1(current_position, lines)
-    print("====PART 1====")
+    # positions, zero_counter = day01_2(current_position, lines)
+    # print("====PART 2====")
     # print(positions)
-    print(zero_counter)
+    # print(zero_counter)
 
-    print("====PART 2====")
-    positions, zero_counter = day01_2(current_position, lines)
-    # print(positions)
-    print(zero_counter)
+    # file_name = "data/aoc2025/input01_puzzle.txt"
+    # lines = read_data_01(file_name)
+    # positions, zero_counter = day01_1(current_position, lines)
+    # print("====PART 1====")
+    # # print(positions)
+    # print(zero_counter)
+
+    # print("====PART 2====")
+    # positions, zero_counter = day01_2(current_position, lines)
+    # # print(positions)
+    # print(zero_counter)
